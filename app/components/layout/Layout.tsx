2a2c076e82756a91d40a7fa8457c20ed6827c240
Pavlin <pavlin.react@gmail.com>
2022-02-19 15:16:14 +0200

    Layout

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,13 +1,16 @@
 import { View, Text, ScrollView } from "react-native";
 import React, { FC } from "react";
+import tw from 'twrnc'
 
 interface ILayout {
   isScrollView?: boolean;
 }
 
+ export let styleCenter = tw`h-full w-full bg-white pt-16`;
+
 const Layout: FC<ILayout> = ({ children, isScrollView = true }) => {
   return (
-    <View>
+    <View style={styleCenter} >
       {isScrollView ? <ScrollView>{children}</ScrollView> : children}
     </View>
   );
--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
-import React, { FC } from 'react'
+import { View, Text } from "react-native";
+import React, { FC } from "react";
+import tw from "twrnc";
+import { styleCenter } from "../../layout/Layout";
 
 const Auth: FC = () => {
   return (
-    <View>
-      <Text>Auth</Text>
+    <View style={styleCenter}>
+      <View style={tw`mx-5 justify-center items-center h-full`}>
+        <View style={tw`w-9/12`} >
+          <Text>Auth</Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
